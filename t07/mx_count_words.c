16030c63a1c2737c2b7c04055d3b6a1c98c2375f
alina2435 <andreievalina2007@gmail.com>
2025-02-06 16:00:48 +0100

    Update mx_count_words.c

--- t07/mx_count_words.c
+++ t07/mx_count_words.c
@@ -1,17 +1,17 @@
 int mx_count_words(const char *str, char delimiter) {
-    if (!str) return -1; // Проверка на NULL
+    if (!str) return -1; 
 
     int count = 0;
-    int in_word = 0; // Флаг внутри слова
+    int in_word = 0; 
 
     while (*str) {
         if (*str != delimiter) {
             if (!in_word) {
-                count++; // Начало нового слова
+                count++; 
                 in_word = 1;
             }
         } else {
-            in_word = 0; // Выходим из слова
+            in_word = 0; 
         }
         str++;
     }
