9588112b7c821ff79b6ecda586d9838a70aed283
Alina Andreeva <alina.andreieva@khpi.edu.ua>
2025-02-06 15:29:01 +0100

    Добавлен Task 02: функция mx_strchr

---
+++ t02/mx_strchr.c
@@ -0,0 +1,10 @@
+char *mx_strchr(const char *s, int c) {
+    while (*s) {
+        if (*s == (char)c) {
+            return (char *)s;
+        }
+        s++;
+    }
+    return (c == '\0') ? (char *)s : 0;
+}
+
