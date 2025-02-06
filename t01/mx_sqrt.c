583775552f1dc91148570a9996e8ff612200f4bd
Alina Andreeva <alina.andreieva@khpi.edu.ua>
2025-02-06 15:25:31 +0100

    Добавлен Task 01: функция mx_sqrt

---
+++ t01/mx_sqrt.c
@@ -0,0 +1,19 @@
+int mx_sqrt(int x) {
+    if (x < 0) {
+        return 0;
+    }
+
+    int left = 1, right = x;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (mid * mid == x) {
+            return mid;
+        } else if (mid * mid < x) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return 0;
+}
+
