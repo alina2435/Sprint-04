d0adbf697f488ee5b25b32f1f00d530e9758fca1
Alina Andreeva <alina.andreieva@khpi.edu.ua>
2025-02-17 23:11:35 +0100

    Initial commit for Sprint 04

---
+++ t02/mx_strchr.c
@@ -0,0 +1,12 @@
+char *mx_strchr(const char *s, int c)
+{
+    while (*s != '\0')
+    {
+        if (*s == c)
+        {
+            return (char *)s;
+        }
+        s++;
+    }
+    return 0;
+}
